74eccb0ce8ebe43c5617b2270b4efa965e18e7d9
Judd Keppel <kepzorz@gmail.com>
2015-04-03 15:45:42 -0700

    bind dispatch shortcut to dispatcher context

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var dispatcher = require('./lib/dispatcher.js')
 module.exports = {
   store: require('./lib/store.js'),
   dispatcher: dispatcher,
-  dispatch: dispatcher.dispatch
+  dispatch: dispatcher.dispatch.bind(dispatcher)
 }
