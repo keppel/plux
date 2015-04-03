b9566324c735e1415d4d588a0d516438ff0165c2
Judd Keppel <kepzorz@gmail.com>
2015-04-03 15:35:57 -0700

    add dispatch shortcut

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+var dispatcher = require('./lib/dispatcher.js')
+
 module.exports = {
   store: require('./lib/store.js'),
-  dispatcher: require('./lib/dispatcher.js')
+  dispatcher: dispatcher,
+  dispatch: dispatcher.dispatch
 }
